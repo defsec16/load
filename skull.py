265f15861cb8625ea1d147dde3be7d6066300241
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-16 06:26:36 +0600

    Create skull.py

---
+++ skull.py
@@ -0,0 +1,80 @@
+import os
+from time import sleep
+def clean():
+  try:
+    os.system('clear')
+  except:
+    os.system('cls')
+clean()
+banner = """                                                                                                             
+                                                                 
+                                      \033[31m                                          
+                                     .,,,,,,,,,,,.                                            
+                                  .',:odddodddoolc;'.                                         
+                               .'cllllloollodoooooolc:;.                                      
+                             .':llllclc:clcloolccloolcc:'.                                    
+                            ';:llc::ccc::c:collc:cl:;;:cc:'.                                  
+                           ;lccclc;;;;;::c::cccclllcclolool,.                                 
+                          'looc;,'....'cc;..';cll;'''';coool,                                 
+                         .:lol,.      .;c.   .c;..     .'ldo:.                                
+                         .:c:'         .:'   .;,         'llc;.                               
+                         ,::;.      ...;'     .c;.       .:cl;.                               
+                        .':c:.   ..';;,'.      ;ol;'.''. .:loc.                               
+                        .,;:c;,,;:llc:;.. .... ,looolooc::cccc,                               
+                       .'::::cc::ccc:cc:;;ccc::clooooodoooc::c:.                              
+                       .';;,',,.':cccccccccclllllooodl;,;;,',lc.                              
+                        .;;,..   .;llc::c:cllllllllo:.    .,cl;.                              
+                        .;;:c:,.  .cc:,;c::clccccool'  .;:;cc:'                               
+                        ..,;cc;.  ;oc;,;:;;cl:;:lolo:...cccc;.                                
+                          .;:ccc,.coc:'.....'...'lolc..;c;;c;.                                
+                          .',::c;';c;..          .'::'.:l:::,.                                
+                            .;c:. ..               .. .co:...                                 
+                             ,c;.                     .co:.                                   
+                            '::;'.                   .;ool,.                                  
+                            .;:::,                   ,lool,                                                                       .;loool;.           .,cllll:.                                    
+                               ,llloc;,..',,''...,c::cc,.                                     
+                                .:clllllllllollcclllc;'                                       
+                                 .'coooolclc:cllcoo:..                                        
+                                   .;lollool:cllloc'                                          
+                                     .:oooooooll;.                                            
+                                      ..........          
+                                   """        
+print(banner)
+sleep(8)
+clean()
+print('\033[32m')
+banner2 = """                                                                                                             
+                                                                 
+                                      \033[32m                                          
+                                      .,,,,,,,,,,,.                                            
+                                  .',:odddodddoolc;'.                                         
+                               .'cllllloollodoooooolc:;.                                      
+                             .':llllclc:clcloolccloolcc:'.                                    
+                            ';:llc::ccc::c:collc:cl:;;:cc:'.                                  
+                           ;lccclc;;;;;::c::cccclllcclolool,.                                 
+                          'looc;,'....'cc;..';cll;'''';coool,                                 
+                         .:lol,.      .;c.   .c;..     .'ldo:.                                
+                         .:c:'         .:'   .;,         'llc;.                               
+                         ,::;.      ...;'     .c;.       .:cl;.                               
+                        .':c:.   ..';;,'.      ;ol;'.''. .:loc.                               
+                        .,;:c;,,;:llc:;.. .... ,looolooc::cccc,                               
+                       .'::::cc::ccc:cc:;;ccc::clooooodoooc::c:.                              
+                       .';;,',,.':cccccccccclllllooodl;,;;,',lc.                              
+                        .;;,..   .;llc::c:cllllllllo:.    .,cl;.                              
+                        .;;:c:,.  .cc:,;c::clccccool'  .;:;cc:'                               
+                        ..,;cc;.  ;oc;,;:;;cl:;:lolo:...cccc;.                                
+                          .;:ccc,.coc:'.....'...'lolc..;c;;c;.                                
+                          .',::c;';c;..          .'::'.:l:::,.                                
+                            .;c:. ..                .. .co:...                                 
+                             ,c;.                     .co:.                                   
+                            '::;'.                   .;ool,.                                  
+                            .;:::,                   ,lool,                                                                       .;loool;.           .,cllll:.                                    
+                               ,llloc;,..',,''...,c::cc,.                                     
+                                .:clllllllllollcclllc;'                                       
+                                 .'coooolclc:cllcoo:..                                        
+                                   .;lollool:cllloc'                                          
+                                     .:oooooooll;.                                            
+                                      ..........             
+                                   """        
+print(banner2)
+sleep(8)
